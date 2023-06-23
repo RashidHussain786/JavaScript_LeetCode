b887fb5255c6b94cf9da8e3e497a2d0b8e5b7a2a
Rashid <rashiddss3@gmail.com>
2023-06-23 15:33:42 +0530

    EvaluateRPN_150

---
+++ Day5/EvaluateRPN_150.js
@@ -0,0 +1,36 @@
+var evalRPN = function(tokens) {
+    const stack=[];
+    for(let token of tokens){
+        if(isOperator(token)){
+            const operand2=stack.pop();
+            const operand1=stack.pop();
+            const result =performOperation(token ,operand1,operand2);
+            stack.push(result);
+        }else{
+            stack.push(Number(token));
+        }
+    }
+    return stack.pop();
+};
+const isOperator=function(token){
+    return token === '+' || token === '-' || token === '*' || token === '/';
+};
+
+const performOperation= function(operator ,operand1,operand2){
+switch (operator) {
+    case '+':
+      return operand1 + operand2;
+    case '-':
+      return operand1 - operand2;
+    case '*':
+      return operand1 * operand2;
+    case '/':
+      return Math.trunc(operand1 / operand2);
+  }
+}
+
+//Example Usages:
+
+const tokens=["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
+
+console.log(evalRPN(tokens));
