a723c289fc2281f0f3d365ea3e19344be4c8dab0
Rashid <rashiddss3@gmail.com>
2023-06-24 12:51:15 +0530

    CarFleet_853

---
+++ Day6/CarFleet_853.js
@@ -0,0 +1,33 @@
+//  we can also use map in place of for 
+// cars.map((pos,index)=>{position:pos,timeToReach: (target-pos)/speed[index]})
+
+//Time Complexity O(n log n)
+//Space Complexity O(n)
+
+var carFleet = function(target, position, speed) {
+   const cars=[]
+   for(let i=0;i<position.length;i++){
+       cars.push({
+           position:position[i],
+           timeToReach:(target-position[i])/speed[i],
+       })
+   }
+   cars.sort((a,b)=>b.position - a.position) // dec order
+   let fleets=0;
+   let maxTime=0;
+   for(let i=0;i<cars.length;i++){
+       let car =cars[i];
+       if(car.timeToReach>maxTime){
+           fleets++;
+           maxTime=car.timeToReach;
+       }
+   }
+   return fleets;
+};
+
+//Example Usages
+const target = 12
+const position = [10,8,0,5,3]
+const speed = [2,4,1,1,3]
+
+console.log(carFleet(target,position,speed))
